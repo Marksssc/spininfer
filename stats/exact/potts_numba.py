12f730d3c925b26a10a39e015b21f6fcadfe7597
Marksss <Mark.R.Schuttelaars@gmail.com>
2026-09-05 23:38:30 +0200

    added a better numba execution for the Potts model

--- stats/exact/potts_numba.py
+++ stats/exact/potts_numba.py
@@ -1,37 +1,43 @@
 import numpy as np
-from numba import njit
+from numba import njit, prange, get_num_threads, get_thread_id
 
-@njit(fastmath=True)
+@njit(fastmath=True, parallel=True)
 def get_exact_statistics(h, J):
-    sites, states = np.shape(h)
+    sites, states = h.shape
     num_states = states**sites
+    n_threads = get_num_threads()
 
-    total_z = 0.0
-    sum_means = np.zeros((sites, states))
-    sum_corrs = np.zeros((sites, sites, states, states))
-    state = np.zeros(sites, dtype=np.int32)
+    sum_means_t = np.zeros((n_threads, sites, states))
+    sum_corrs_t = np.zeros((n_threads, sites, sites, states, states))
+    total_z_t = np.zeros(n_threads)
+    state_t = np.zeros((n_threads, sites), dtype=np.int32)
 
-    for ii in range(num_states):
-        temp = ii
+    powers = np.zeros(sites, dtype=np.int64)
+    for jj in range(sites):
+        powers[jj] = states ** jj
 
-        for jj in range(sites):
-            state[jj] = temp % states
-            temp = temp//states
+    for ii in prange(num_states):
+        tid = get_thread_id()
         
+        for jj in range(sites):
+            state_t[tid, jj] = (ii // powers[jj]) % states
+
         energy = 0.0
         for site in range(sites):
-            energy -= h[site, state[site]]
+            energy -= h[site, state_t[tid, site]]
             for site2 in range(sites):
                 if site == site2:
                     continue
-                energy -= 0.5*J[site, site2, state[site], state[site2]]
-    
-        weight = np.exp(-energy)
-        total_z += weight
+                energy -= 0.5 * J[site, site2, state_t[tid, site], state_t[tid, site2]]
 
+        weight = np.exp(-energy)
+        total_z_t[tid] += weight
         for site in range(sites):
-            sum_means[site, state[site]] += weight
+            sum_means_t[tid, site, state_t[tid, site]] += weight
             for site2 in range(sites):
-                sum_corrs[site, site2, state[site], state[site2]] += weight
-    
-    return sum_means/total_z, sum_corrs/total_z
+                sum_corrs_t[tid, site, site2, state_t[tid, site], state_t[tid, site2]] += weight
+
+    total_z = np.sum(total_z_t)
+    sum_means = np.sum(sum_means_t, axis=0)
+    sum_corrs = np.sum(sum_corrs_t, axis=0)
+    return sum_means / total_z, sum_corrs / total_z
